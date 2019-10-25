596c8e20d7431e4a890748a58e23cf01111978d5
Malar00 <56561977+Malar00@users.noreply.github.com>
2019-10-25 11:45:40 +0200

    Beta (#3)
    * Delete UkosProjektKTJ.sln
    
    * Delete UkosProjektKTJ.vcxproj
    
    * Delete UkosProjektKTJ.vcxproj.filters
    
    * Added decToBin n reverseInt
    
    * Added decToBin n reverseInt

--- UkosProjektKTJ/UkosProjektKTJ.cpp
+++ UkosProjektKTJ/UkosProjektKTJ.cpp
@@ -1,6 +1,40 @@
-﻿
+#include <string>
 #include <iostream>
+using namespace std;
 
+int decToBin(int num);
+int reverseInt(int num);
+
+int main()
+{
+
+	
+	
+}
+
+int reverseInt(int num)
+{
+	int revnum = 0;
+	while (num > 0)
+	{
+		revnum = revnum * 10 + num % 10;
+		num = num / 10;
+	}
+	return revnum;
+}
+int decToBin(int num)
+{
+	int wyn = 0;
+	int i = 10;
+	while (num != 0)
+	{
+		wyn = wyn * 10;
+		wyn = wyn + num % 2;
+		num = num / 2;
+	}
+	wyn = reverseInt(wyn);
+	return wyn;
+}
 int binToDec(string bin)
 {
 	int d = 0;
@@ -13,8 +47,3 @@ int binToDec(string bin)
 	return d;
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
-	
-}
