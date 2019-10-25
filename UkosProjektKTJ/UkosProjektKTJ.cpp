66c4ee965bfe955f316d60de13f74446a23eaf6b
Jan Sitarski <s20701@pjwstk.edu.pl>
2019-10-25 11:36:27 +0200

    Added decToBin n reverseInt

--- UkosProjektKTJ/UkosProjektKTJ.cpp
+++ UkosProjektKTJ/UkosProjektKTJ.cpp
@@ -1,8 +1,38 @@
-﻿
+﻿#include <string>
 #include <iostream>
+
+using namespace std;
+
+int decToBin(int num);
+int reverseInt(int num);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+	
 }
 
+int reverseInt(int num)
+{
+	int revnum = 0;
+	while (num > 0)
+	{
+		revnum = revnum * 10 + num % 10;
+		num = num / 10;
+	}
+	return revnum;
+}
+int decToBin(int num)
+{
+	int wyn = 0;
+	int i = 10;
+	while (num != 0)
+	{
+		wyn = wyn * 10;
+		wyn = wyn + num % 2;
+		num = num / 2;
+	}
+	wyn = reverseInt(wyn);
+	return wyn;
+}
