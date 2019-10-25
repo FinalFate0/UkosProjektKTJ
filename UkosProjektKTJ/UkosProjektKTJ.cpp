46d7b5490fdc9b018174600ba295ba5f18332140
Konstanty Kamien <miecio987@gmail.com>
2019-10-25 11:24:47 +0200

    asdasdsad

--- UkosProjektKTJ/UkosProjektKTJ.cpp
+++ UkosProjektKTJ/UkosProjektKTJ.cpp
@@ -1,8 +1,20 @@
 ﻿
 #include <iostream>
 
+int binToDec(string bin)
+{
+	int d = 0;
+	int n = bin.size();
+	for (int i = 0; i < n; i++)
+	{
+		d *= 2;
+		d += bin[i] - '0';
+	}
+	return d;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
+	
 }
-
